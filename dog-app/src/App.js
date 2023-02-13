816b14859f4a4b97e4fe2b1fbf49d441acff2ac1
Sandra Reinecke <sandra.reinecke@consid.se>
2023-02-13 20:29:49 +0100

    fix

--- dog-app/src/App.js
+++ dog-app/src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Element from "./pages/Element";
+import NoPage from "./pages/NoPage"
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="element" element={<Element />} />
+              <Route path="*" element={<NoPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </header>
     </div>
   );
---
+++ dog-app/src/pages/Element.js
@@ -0,0 +1,6 @@
+const Element = () => {
+    return <h1>Contact Me</h1>;
+  };
+  
+  export default Element;
+  
