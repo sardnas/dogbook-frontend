54262f71c44dd886cead101c089339ead40ea468
Adam Tovatt <sporedenstore@hotmail.com>
2023-02-13 21:47:34 +0100

    add code for fetching and displaying data from an api

---
+++ dog-app/src/Api.js
@@ -0,0 +1,56 @@
+//will get a breed from the webapi based on breed id
+export async function GetBreedById(breedId) {
+  return await fetch(GetBasePath() + "/api/breeds/" + breedId, {
+    method: "GET",
+  });
+}
+
+//will get a list of available breeds
+export async function GetBreeds() {
+  return await fetch(GetBasePath() + "/api/breeds", {
+    method: "GET",
+  });
+}
+
+//will create a new breed
+export async function CreateBreed(name, color, trainability) {
+  return await fetch(GetBasePath() + "/api/breeds/create", {
+    method: "POST",
+    body: JSON.stringify({ name, color, trainability }),
+    headers: { "Content-Type": "application/json" },
+  });
+}
+
+//below is code to make it easier to have one base path when you are developing locally and one when the site is published
+//it is because maybe you want to use a locally hosted webapi when developing to make it easier to debug
+//but obviously you don't want to use that locally hosted webapi when you have published the site
+
+const publishedBasePath = "https://obviouslyfakedomainname.se/dog-info-api";
+const localBasePath = "https://localhost:5001"; // <---- change this to the path of your locally hosted webapi (!!)
+
+let requestBasePath = null;
+
+export function CreateBasePath() {
+  requestBasePath = publishedBasePath; //this should be the path to the api when it is published
+
+  //this if-statement will check if this is running as development process
+  if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+    //use local address if development
+    requestBasePath = localBasePath; //whatever path you're hosting your api locally on
+
+    //you can comment out the line of code above if you want to use the production webapi when developing locally
+
+    //care of the s in "https". It can cause problems, at the same time, only having "http" can also cause problems
+    //"http" can cause problems because mixed content loading is not allowed
+    //"https" can cause problems because there needs to be a valid and trusted ssl certificate
+    //Sometimes this can appear as a CORS error when it's actually a SSL error
+  }
+}
+
+export function GetBasePath() {
+  if (!requestBasePath) {
+    CreateBasePath(); //if there is no base path created yet we will create it now
+  }
+
+  return requestBasePath; //return the base path
+}
--- dog-app/src/pages/Home.js
+++ dog-app/src/pages/Home.js
@@ -1,6 +1,68 @@
+import { useState, useEffect } from "react";
+import { GetBreeds } from "../Api";
+
 const Home = () => {
-    return <h1>Home</h1>;
-  };
-  
-  export default Home;
-  
+  const [shouldFetchBreeds, setShouldFetchBreeds] = useState(true); //true because it should fetch the breeds when the page loads
+  const [errorFetchingBreeds, setErrorFetchingBreeds] = useState(false); //false because no error has occured when the page loads
+  const [breeds, setBreeds] = useState(null); //null because there is no breeds data yet
+
+  useEffect(() => {
+    //this is a function that will call the api function for fetching breeds and handle the response
+    async function FetchBreeds() {
+      try {
+        let response = await GetBreeds();
+
+        //check if the response was a sucess, 200 means that it was
+        if (response.status === 200) {
+          let json = await response.json(); //we want to parse the response body as json if it was a sucess
+
+          console.log(json); //don't publish code with this line! I just added it here to show that
+          //it can be quite usefull to write the value of the json object to the console
+          //you could try to write out the value of response too if there is something that is not working
+
+          setBreeds(json); //save the data we have gotten
+        } else if (response.status === 400) {
+          //we can check for specific error codes like this
+          alert(
+            "400 means that the server thinks this request was invalid because of missing or invalid input"
+          );
+          setErrorFetchingBreeds(true); //set error value
+        } else {
+          alert("An unknown error occured");
+          setErrorFetchingBreeds(true); //set error value
+        }
+      } catch (e) {
+        console.error("There was an error when fetching the breeds! :-(");
+        console.error(e);
+        setErrorFetchingBreeds(true); //set error value
+      }
+    }
+
+    //it is important to remember to actually call the function declared above :weary:
+    if (shouldFetchBreeds) {
+      setShouldFetchBreeds(false);
+      FetchBreeds();
+    }
+  }, [shouldFetchBreeds, breeds]); //this is a dependency array, it tells react what values it should watch for updates
+
+  console.log(errorFetchingBreeds);
+  return (
+    <>
+      <h1>Home</h1>
+      {breeds ? ( //if breeds exist we want to display them
+        breeds.listOfBreeds.map((oneBreed, index) => {
+          //look up "javascript map" for more information about this syntax. It takes a list of something and then returns something for each item in the list
+          return <div key={index}>there's a breed called: {oneBreed.name}</div>;
+        })
+      ) : errorFetchingBreeds ? ( //if breeds don't exist we display a message based on if there has been an error or not
+        <div>
+          something went terribly, terribly wrong when fetching breeds 😞
+        </div>
+      ) : (
+        <div>loading breeds probably... ✋🤠👉⌛</div>
+      )}
+    </>
+  );
+};
+
+export default Home;
