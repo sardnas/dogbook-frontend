4a56163c07cfb9a835f3aeb240dcd2c4a2b2ab67
Sandra Reinecke <sandra.reinecke@consid.se>
2023-02-14 15:12:24 +0100

    fixed dogtable.css

--- dog-app/src/functions/DogTable.js
+++ dog-app/src/functions/DogTable.js
@@ -1,21 +1,23 @@
+import '../styles/DogTable.css';
+
 function DogTable({theadData, tbodyData}){
     console.log('DogTable: ');
     console.log(theadData);
 
   return (
-       <table>
+       <table className="table">
            <thead>
               <tr>
                {theadData.map(heading => {
-                 return <th key={heading}>{heading}</th>
+                 return <th className="th" key={heading}>{heading}</th>
                })}
              </tr>
            </thead>
            <tbody>
                {tbodyData.map((row, index) => {
-                   return <tr key={index}>
+                   return <tr className='tr' key={index}>
                        {theadData.map((key, index) => {
-                            return <td key={row[key]}>{row[key]}</td>
+                            return <td className="td" key={row[key]}>{row[key]}</td>
                        })}
                  </tr>;
                })}
