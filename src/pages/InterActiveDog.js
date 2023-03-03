18c1f1ab9c2f8e1c3e95f1ca2e1cd42024d59213
Sandra Reinecke <sandra.reinecke@consid.se>
2023-03-03 16:34:02 +0100

    fix

--- src/pages/InterActiveDog.js
+++ src/pages/InterActiveDog.js
@@ -2,7 +2,7 @@ import { useRef, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 
-function Box(props) {
+function Dog(props) {
     // This reference gives us direct access to the THREE.Mesh object
     const ref = useRef()
     // Hold state for hovered and clicked events
@@ -19,7 +19,7 @@ function Box(props) {
             onClick={(event) => click(!clicked)}
             onPointerOver={(event) => hover(true)}
             onPointerOut={(event) => hover(false)}>
-            <boxGeometry args={[1, 1, 1]} />
+            <octahedronGeometry args={[1, 2, -2, 1, 2, 1, 1, 1]} />
             <meshStandardMaterial color={hovered ? 'hotpink' : '#7180B9'} />
         </mesh>
     )
@@ -31,7 +31,7 @@ export default function InterActiveDog() {
             <ambientLight intensity={0.5} />
             <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
             <pointLight position={[-10, -10, -10]} />
-            <Box position={[-1, 0, 0]} />
+            <Dog position={[-1, 0, 0]} />
             <OrbitControls />
         </Canvas>
     )
